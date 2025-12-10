50ba64ee8832e4e28c2191657c923fb50b1a52a4
AmeyaMandwale <166429225+AmeyaMandwale@users.noreply.github.com>
2025-12-10 10:52:59 +0530

    Clean up LoginPage component imports
    Removed commented-out testing code and added useContext import.

--- LoginPage.tsx
+++ LoginPage.tsx
@@ -1,8 +1,6 @@
 
-
-//Testing code//
 import { useState } from "react";
-
+import { useContext } from "react";
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
